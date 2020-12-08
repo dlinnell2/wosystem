780d6c060d8e159bf39ab1f7c9fd6a621259a266
Donald Linnell <donald.linnell@gmail.com>
2020-12-07 22:09:08 -0600

    WO Schema

---
+++ models/WO.js
@@ -0,0 +1,54 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var WOSchema = new Schema({
+
+    description: {
+        type: String,
+        required: true
+    },
+    assignedTo: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    submittedBy: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    actionsTaken: String,
+    notes: String,
+    laborHours: [
+            {
+                employee: {
+                    type: String,
+                    required: true
+                },
+                date: {
+                    type: String,
+                    required: true
+                },
+                hours: {
+                    type: Number,
+                    required: true
+                }
+            }
+        ]
+
+
+}, {
+    timestamps: true
+});
+
+
+var WO = mongoose.model("WO", WOSchema);
+
+module.exports = WO;
