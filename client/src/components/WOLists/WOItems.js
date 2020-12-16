71a9d659a955a19b257ba21df4a35b0b7bde9c2a
Donald Linnell <donald.linnell@gmail.com>
2020-12-16 16:11:21 -0600

    filtering values from returned wo object based on array contained in state

--- client/src/components/WOLists/WOItems.js
+++ client/src/components/WOLists/WOItems.js
@@ -14,25 +14,19 @@ class WOItems extends React.Component {
 
     createWOItems = () => {
 
-        Object.filter = (obj, predicate) =>
-            Object.assign(...Object.keys(obj)
-                .filter(key => predicate(obj[key]))
-                .map(key => ({ [key]: obj[key] })));
+        Object.filter = (obj) =>
+            Object.keys(obj)
+                .filter(key => this.state.categoriesToShow.indexOf(key) >= 0)
+                .map(key => obj[key]);
 
-        let order = Object.assign({}, this.props.order);
-        order._id = order._id.slice(-5);
+        let order = {...this.props.order}
+        order._id=order._id.slice(-5);
 
-        console.log(Object.assign(...Object.keys(order)))
+        let filteredOrder = Object.filter(order);
 
-        let filteredOrder = Object.filter(order, item => this.state.categoriesToShow.indexOf(item >= 0))
-
-        console.log(filteredOrder)
-
-        return (
-            Object.values(filteredOrder).map((item, index) => (
-                <Col key={index} className="wotable">{item}</Col>
-            ))
-        )
+        return filteredOrder.map((item, index) => (
+            <Col key={index} className="wotable">{item}</Col>
+        ))
     }
 
     render() {
