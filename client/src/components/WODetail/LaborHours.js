304118f9022f7c351aa9127c69c9b85c44a64979
Donald Linnell <donald.linnell@gmail.com>
2020-12-20 15:00:16 -0600

    changing labor hours to class component, working on creating rows/columns for entries

--- client/src/components/WODetail/LaborHours.js
+++ client/src/components/WODetail/LaborHours.js
@@ -1,80 +1,114 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import { Row, Col, Form, Card, Button, Modal } from 'react-bootstrap';
 import "./wodetail.css"
 
-const LaborHours = props => {
+class LaborHours extends Component {
 
-    const [show, setShow] = useState(false);
+    constructor(props) {
+        super()
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    }
 
-    const sendLaborHours = () => {
-        props.addLaborHours().then( () => {
+    state = {
+        show:false
+    };
+
+    setShow = (value) => {
+        this.setState({
+            show:value
+        })
+    }
+
+    handleClose = () => setShow(false);
+    handleShow = () => setShow(true);
+
+    sendLaborHours = () => {
+        props.addLaborHours().then(() => {
             handleClose()
         })
     }
 
-    return (
+    createEntryRow = () => {
+        props.laborHours.map((entry, index) => (
+            <Row key={index}>
+                {this.createEntryColumns(entry)}
+            </Row>
+        ))
+    }
 
-        <>
-            <Card className="detailCard">
-                <Card.Header>Labor Hours</Card.Header>
-                <Card.Body>
-                    <Row>
-                        <Col>
-                            <Button
-                                variant="outline-secondary"
-                                className="button"
-                                size="sm"
-                                onClick={handleShow}>Add New</Button>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>Name</Col>
-                        <Col>Date</Col>
-                        <Col>Hours</Col>
-                    </Row>
-                </Card.Body>
-            </Card>
+    createEntryColumns = entry => {
+        let adjusted = entry.shift();
+        console.log(adjusted)
+        adjusted.map((item, index) = (
+            <Col key={index}>{entry[item]}</Col>
+        ))
+    }
 
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>New Labor Entry</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Form>
-                        <Form.Group>
-                            <Form.Label>Employee Name</Form.Label>
-                            <Form.Control
-                                name="laborName"
-                                value={props.laborName}
-                                onChange={props.handleInputChange}
-                            />
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Label>Date</Form.Label>
-                            <Form.Control
-                                name="laborDate"
-                                value={props.laborDate}
-                                onChange={props.handleInputChange}
-                            />
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Label>Hours worked</Form.Label>
-                            <Form.Control
-                                name="laborTime"
-                                value={props.laborTime}
-                                onChange={props.handleInputChange}
-                            />
-                        </Form.Group>
-                    </Form>
-                    <Button variant="success" onClick={sendLaborHours}>Add</Button>
-                </Modal.Body>
-            </Modal>
+    render() {
 
-        </>
-    )
+        return (
+
+            <>
+                <Card className="detailCard">
+                    <Card.Header>Labor Hours</Card.Header>
+                    <Card.Body>
+                        <Row>
+                            <Col>
+                                <Button
+                                    variant="outline-secondary"
+                                    className="button"
+                                    size="sm"
+                                    onClick={handleShow}>Add New</Button>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>Name</Col>
+                            <Col>Date</Col>
+                            <Col>Hours</Col>
+                        </Row>
+                        {this.createEntryRow()}
+                    </Card.Body>
+                </Card>
+
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>New Labor Entry</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form>
+                            <Form.Group>
+                                <Form.Label>Employee Name</Form.Label>
+                                <Form.Control
+                                    name="laborName"
+                                    value={props.laborName}
+                                    onChange={props.handleInputChange}
+                                />
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label>Date</Form.Label>
+                                <Form.Control
+                                    name="laborDate"
+                                    value={props.laborDate}
+                                    onChange={props.handleInputChange}
+                                />
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label>Hours worked</Form.Label>
+                                <Form.Control
+                                    name="laborTime"
+                                    value={props.laborTime}
+                                    onChange={props.handleInputChange}
+                                />
+                            </Form.Group>
+                        </Form>
+                        <Button variant="success" onClick={sendLaborHours}>Add</Button>
+                    </Modal.Body>
+                </Modal>
+
+            </>
+        )
+    }
+
 }
 
 export default LaborHours
