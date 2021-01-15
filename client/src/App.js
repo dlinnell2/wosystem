abca6200518b0dc818c6e777a75306621246a553
Donald Linnell <donald.linnell@gmail.com>
2021-01-15 10:15:30 -0600

    adding link to navbar

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,18 @@ import './App.css';
 import { WOList, AddWO, WODetail, AddAsset } from './pages'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from 'react-bootstrap/Navbar'
+import { Navbar, Nav } from 'react-bootstrap'
 
 class App extends Component {
 
   render() {
     return (
       <div>
-        <Navbar bg="dark" variant="dark" expand="sm">
-          <Navbar.Brand href="/">Home</Navbar.Brand>
+        <Navbar bg="dark" variant="dark" expand="sm" sticky="top" className="navbar">
+          <Navbar.Brand>WO System</Navbar.Brand>
+          <Nav>
+            <Nav.Link href="/">Order List</Nav.Link>
+          </Nav>
         </Navbar>
         <Switch>
           <Route path="/addOrder">
