c941b37f6946739083edd3bdf34fc111081579da
Donald Linnell <donald.linnell@gmail.com>
2021-01-16 14:35:23 -0600

    adding nav links to bar

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,10 @@ class App extends Component {
         <Navbar bg="dark" variant="dark" expand="sm" sticky="top" className="navbar">
           <Navbar.Brand>WO System</Navbar.Brand>
           <Nav>
-            <Nav.Link href="/">Order List</Nav.Link>
+            <Nav.Link href="/">Orders</Nav.Link>
+            <Nav.Link href="/asset">Assets</Nav.Link>
+            <Nav.Link href="/user">Users</Nav.Link>
+            <Nav.Link href="/location">Locations</Nav.Link>
           </Nav>
         </Navbar>
         <Switch>
