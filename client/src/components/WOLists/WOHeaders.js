c27b2eeade2c879bea03342f6173e7fde9ca2bbf
Donald Linnell <donald.linnell@gmail.com>
2021-01-20 15:53:12 -0600

    wodetail not pulling up order, working to figure out why

--- client/src/components/WOLists/WOHeaders.js
+++ client/src/components/WOLists/WOHeaders.js
@@ -25,7 +25,7 @@ class WOHeaders extends React.Component {
         return (
             <Row>
                 <Col className="buttonColumn">
-                    <LinkButton to="/addOrder" variant="success" block className="listButton">
+                    <LinkButton to="/order/add" variant="success" block className="listButton">
                         New
                     </LinkButton>
                 </Col>
--- client/src/components/WOLists/WOItems.js
+++ client/src/components/WOLists/WOItems.js
@@ -48,15 +48,15 @@ class WOItems extends React.Component {
     }
 
     render() {
-
+        console.log(this.props)
         return (
             <Row>
-                <Col className="buttonColumn"> 
-                    <LinkButton 
-                    to={"/detail/" + this.props.order._id}
-                    variant="outline-primary"
-                    block
-                    className="detailButton">Detail
+                <Col className="buttonColumn">
+                    <LinkButton
+                        to={"/order/detail/" + this.props.order._id}
+                        variant="outline-primary"
+                        block
+                        className="detailButton">Detail
                     </LinkButton>
                 </Col>
                 {this.createWOItems()}
