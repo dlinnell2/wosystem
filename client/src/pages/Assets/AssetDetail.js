2ce741e00cf38eaf9677aae51a9b6e4df9d667da
Donald Linnell <donald.linnell@gmail.com>
2021-01-24 21:13:34 -0600

    file structure for asset details, inserting link buttons

--- client/src/pages/Assets/AssetDetail.js
+++ client/src/pages/Assets/AssetDetail.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
 import API from "../../utils/API";
+import { Container, Row, Col, Form, Card, Button } from 'react-bootstrap';
+import { LinkButton } from "../../components/General"
+import "./assetStyling.css"
 
 class AssetDetail extends Component {
 
     constructor(props) {
         super()
+    }
+
+    state ={
+        variant: 'secondary',
+        message: ''
     }
 
     componentDidMount() {
@@ -17,7 +25,23 @@ class AssetDetail extends Component {
 
     render() {
         return (
-            <h2>Detail</h2>
+            <Container fluid>
+                <Row>
+                    <Col sm={4}>
+                        <Button
+                            variant={this.state.variant}
+                            className="detailButton"
+                            size="sm">Save Updates</Button>
+
+                        <LinkButton
+                            to="/assets/"
+                            variant="primary"
+                            className="detailButton"
+                            size="sm">Return to list</LinkButton>
+                    </Col>
+                    <Col sm={2}>{this.state.message}</Col>
+                </Row>
+            </Container>
         )
     }
 }
