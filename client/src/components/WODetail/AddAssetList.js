1c356f25c0b6c15dfe97f4e144986fbd7d05afc1
Donald Linnell <donald.linnell@gmail.com>
2021-01-29 22:09:03 -0600

    asset list displaying on card

--- client/src/components/WODetail/AddAssetList.js
+++ client/src/components/WODetail/AddAssetList.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import API from '../../utils/API';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const AddAssetList = props => {
 
     const [assets, updateAssets] = useState([])
 
-    useEffect(() => { getAssets() },[])
+    useEffect(() => { getAssets() }, [])
 
     const getAssets = () => {
-        console.log('ping')
         API.getAll('assets')
             .then((res) => {
                 updateAssets(res.data)
@@ -16,11 +16,20 @@ const AddAssetList = props => {
     }
 
     return (
-        <div>
-            {assets.map((a,id) => (
-                <p>Hello</p>
+        <Container>
+            <Row>
+                <Col>Name</Col>
+                <Col>Type</Col>
+                <Col>Subcategory</Col>
+            </Row>
+            {assets.map((asset, index) => (
+                <Row id={index}>
+                    <Col>{asset.name}</Col>
+                    <Col>{asset.type}</Col>
+                    <Col>{asset.subcategory}</Col>
+                </Row>
             ))}
-            </div>
+        </Container>
     )
 
 }
