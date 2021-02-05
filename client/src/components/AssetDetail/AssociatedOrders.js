6a5c930189726fa6e0fd791e9016052587698d72
Donald Linnell <donald.linnell@gmail.com>
2021-02-05 15:35:06 -0600

    adjusting css

--- client/src/components/AssetDetail/AssociatedOrders.js
+++ client/src/components/AssetDetail/AssociatedOrders.js
@@ -4,50 +4,30 @@ import { LinkButton } from '../General'
 
 const AssociatedOrders = props => {
 
-    const createRows = () => {
-        props.orderData.map((order, index) => (
-
-            <Row>
-                <Col>
-                    <LinkButton
-                        to={"/orders/detail/" + order._id}
-                        variant="outline-primary"
-                        block
-                        className="detailButton">Detail
-                    </LinkButton>
-                </Col>
-                <Col>{order.id}</Col>
-                <Col>{order.description}</Col>
-                <Col>{order.status}</Col>
-            </Row>
-
-        ))
-    }
-
     return (
         <Card className='detailCard'>
             <Card.Header>Associated Orders</Card.Header>
             <Card.Body>
                 <Row>
-                    <Col />
-                    <Col>ID</Col>
-                    <Col>Description</Col>
-                    <Col>Status</Col>
+                    <Col lg={2}/>
+                    <Col lg={2}>ID</Col>
+                    <Col lg={6}>Description</Col>
+                    <Col lg={2}>Status</Col>
                 </Row>
                 {props.orderData.map((order, index) => (
 
-                    <Row>
-                        <Col className="buttonColumn">
+                    <Row key={index}>
+                        <Col key={index} className="buttonColumn" lg={2}>
                             <LinkButton
                                 to={"/orders/detail/" + order._id}
                                 variant="outline-primary"
-                                size="sm"
-                                className="detailButton">Detail
+                                className="detailButton"
+                                key={`button${index}`}>Detail
                             </LinkButton>
                         </Col>
-                        <Col>{order.id}</Col>
-                        <Col>{order.description}</Col>
-                        <Col>{order.status}</Col>
+                        <Col key={`id${index}`} lg={2}>{order.id}</Col>
+                        <Col key={`descrip${index}`} lg={6}>{order.description}</Col>
+                        <Col key={`status${index}`} lg={2}>{order.status}</Col>
                     </Row>
 
                 ))}
