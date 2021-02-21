69e4359f0617fd855ff242f5dd7df5d7d275d4cb
Donald Linnell <donald.linnell@gmail.com>
2021-02-20 19:44:38 -0600

    beginning to build form to add user, input changes functioning correctly

--- client/src/pages/Assets/AddAsset.js
+++ client/src/pages/Assets/AddAsset.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import API from "../../utils/API";
-import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+import { Row, Col, Form, Button } from 'react-bootstrap'
 import { LinkButton } from "../../components/General"
 
 class AddAsset extends Component {
@@ -53,66 +53,64 @@ class AddAsset extends Component {
 
     render() {
         return (
-            <Container>
-                <Form>
-                    <Row>
-                        <Col>
-                            <Form.Group>
-                                <Form.Label>Please enter the asset information below. </Form.Label>
-                                <Form.Control
-                                    placeholder="Enter Asset Name"
-                                    name="name"
-                                    value={this.state.name}
-                                    onChange={this.handleInputChange}
-                                />
-                            </Form.Group>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <Form.Group>
-                                <Form.Control
-                                    placeholder="Asset Type"
-                                    name="type"
-                                    value={this.state.type}
-                                    onChange={this.handleInputChange}
-                                />
-                            </Form.Group>
-                        </Col>
-                        <Col>
-                            <Form.Group>
-                                <Form.Control
-                                    placeholder="Asset Subcategory"
-                                    name="subcategory"
-                                    value={this.state.subcategory}
-                                    onChange={this.handleInputChange}
-                                />
-                            </Form.Group>
-                        </Col>
-                        <Col>
-                            <Form.Group>
-                                <Form.Control
-                                    placeholder="Asset Location"
-                                    name="location"
-                                    value={this.state.location}
-                                    onChange={this.handleInputChange}
-                                />
-                            </Form.Group>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col sm={2}>
-                            <Button variant="success" onClick={this.addNew}>Submit your order</Button>
-                        </Col>
-                        <Col sm={2}>
-                            <LinkButton variant="primary" to="/assets/">Return to list</LinkButton>
-                        </Col>
-                        <Col sm={8}>
-                            <h4>{this.state.status}</h4>
-                        </Col>
-                    </Row>
-                </Form>
-            </Container>
+            <Form>
+                <Row>
+                    <Col>
+                        <Form.Group>
+                            <Form.Label>Please enter the asset information below. </Form.Label>
+                            <Form.Control
+                                placeholder="Enter Asset Name"
+                                name="name"
+                                value={this.state.name}
+                                onChange={this.handleInputChange}
+                            />
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Form.Group>
+                            <Form.Control
+                                placeholder="Asset Type"
+                                name="type"
+                                value={this.state.type}
+                                onChange={this.handleInputChange}
+                            />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group>
+                            <Form.Control
+                                placeholder="Asset Subcategory"
+                                name="subcategory"
+                                value={this.state.subcategory}
+                                onChange={this.handleInputChange}
+                            />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group>
+                            <Form.Control
+                                placeholder="Asset Location"
+                                name="location"
+                                value={this.state.location}
+                                onChange={this.handleInputChange}
+                            />
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={2}>
+                        <Button variant="success" onClick={this.addNew}>Submit your order</Button>
+                    </Col>
+                    <Col sm={2}>
+                        <LinkButton variant="primary" to="/assets/">Return to list</LinkButton>
+                    </Col>
+                    <Col sm={8}>
+                        <h4>{this.state.status}</h4>
+                    </Col>
+                </Row>
+            </Form>
         );
     }
 }
--- client/src/pages/Users/AddUser.js
+++ client/src/pages/Users/AddUser.js
@@ -1,4 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import API from "../../utils/API";
+import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+import { LinkButton } from "../../components/General"
 
 class AddUser extends Component {
 
@@ -7,9 +10,46 @@ class AddUser extends Component {
 
     }
 
-    render (){
+    state = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        location: '',
+        role: '',
+    }
+
+    handleInputChange = (e) => {
+        const { name, value } = e.target;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    render() {
         return (
-            <h2>Add User page</h2>
+            <Form>
+                <Form.Row>
+                    <Form.Label>Please enter the user information below. </Form.Label>
+                </Form.Row>
+                <Form.Row>
+                    <Form.Group as={Col}>
+                        <Form.Control
+                            placeholder="First Name"
+                            name="firstName"
+                            value={this.state.firstName}
+                            onChange={this.handleInputChange}
+                        />
+                    </Form.Group>
+                    <Form.Group as={Col}>
+                        <Form.Control
+                            placeholder="Last Name"
+                            name="lastName"
+                            value={this.state.lastName}
+                            onChange={this.handleInputChange}
+                        />
+                    </Form.Group>
+                </Form.Row>
+            </Form>
         )
     }
 }
