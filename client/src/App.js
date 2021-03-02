7ea7a3c54d584aed0b78842915a05ac5eef36f63
Donald Linnell <donald.linnell@gmail.com>
2021-03-02 16:21:26 -0600

    changing location of router to within app rather than wrapping, adding history. beginning to build context

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routing } from './utils'
 import { Navigation } from './components/General'
 import Container from 'react-bootstrap/Container'
+import { BrowserRouter as Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+const history = createBrowserHistory();
 
 class App extends Component {
 
   render() {
     return (
-      <div>
+      <Router history={history}>
         <Navigation />
         <Container fluid>
           <Routing />
         </Container>
-      </div>
+      </Router>
     );
   }
 }
